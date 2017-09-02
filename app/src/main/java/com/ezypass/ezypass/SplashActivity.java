2f3646eae0b92a7f8acb0817273fa714b59d6cd4
Paul Bouquet <paul.bouquet@gmail.com>
2017-09-02 17:56:18 +0200

    Add welcome activity and bug fixes

---
+++ app/src/main/java/com/ezypass/ezypass/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.ezypass.ezypass;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ezypass.ezypass.welcome.MainWelcomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AppPreferences appPreferences = new AppPreferences(getBaseContext());
+        try {
+            appPreferences.getUserKey();
+            Log.d(NewActivity.class.getName(), "User had a key");
+            startMainActivity();
+        } catch(Exception e){
+            Log.w(NewActivity.class.getName(), "User had no key");
+            startNewActivity();
+            startWelcomeActivity();
+        }
+    }
+
+    /**
+     * Start activity main
+     */
+    private void startMainActivity() {
+        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    /**
+     * Start welcome main
+     */
+    private void startWelcomeActivity() {
+        Intent i = new Intent(SplashActivity.this, MainWelcomeActivity.class);
+        startActivity(i);
+    }
+
+    /**
+     * Start new main
+     */
+    private void startNewActivity() {
+        Intent i = new Intent(SplashActivity.this, NewActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/ezypass/ezypass/welcome/MainWelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.ezypass.ezypass.welcome;
+
+import com.ezypass.ezypass.R;
+import com.stephentuso.welcome.BasicPage;
+import com.stephentuso.welcome.TitlePage;
+import com.stephentuso.welcome.WelcomeActivity;
+import com.stephentuso.welcome.WelcomeConfiguration;
+
+public class MainWelcomeActivity extends WelcomeActivity {
+
+    @Override
+    protected WelcomeConfiguration configuration() {
+        return new WelcomeConfiguration.Builder(this)
+                .defaultBackgroundColor(R.color.colorPrimary)
+                .page(new TitlePage(R.drawable.ic_info_black_24dp, "Welcome to EzyPass"))
+                .page(new BasicPage(R.drawable.ic_info_black_24dp, "No registration", "Your own key will generate all your password extensions"))
+                .page(new BasicPage(R.drawable.ic_info_black_24dp, "Secure", "No server, no account, just remember your key"))
+                .page(new BasicPage(R.drawable.ic_info_black_24dp, "Ezy to use", "Just enter the name of the website or application to secure your passwords."))
+                .build();
+    }
+}
